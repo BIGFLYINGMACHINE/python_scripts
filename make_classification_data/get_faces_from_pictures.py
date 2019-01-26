4b2d09fcdf5f59d478c4d4a2ce916698bcb8f646
Outofnothing <chriszhou0628@gmail.com>
2019-01-26 14:16:26 +0800

    as the name suggests

--- make_classification_data/get_faces_from_pictures.py
+++ make_classification_data/get_faces_from_pictures.py
@@ -3,11 +3,13 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier("/home/zack/total/python_scripts/make_classification_data/haarcascade_frontalface_default.xml")
 
-write_dir = "./write_dir"
-read_dir = "/home/zack/Pictures/Shiyuan_Limei/raw"
+write_dir = "/home/zack/Pictures/Ishihara_Satomi/write_dir"
+read_dir = "/home/zack/Pictures/Ishihara_Satomi/raw"
+
 
 import os
-
+if not os.path.exists(write_dir):
+    os.makedirs(write_dir)
 index = 1
 files = os.listdir(read_dir)
 for file in files:
@@ -19,11 +21,16 @@ for file in files:
                 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
                 for (x, y, w, h) in faces:
-                    img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                    
                     roi_gray = gray[y:y + h, x:x + w]
                     roi_color = img[y:y + h, x:x + w]
                     roi_color=cv2.resize(roi_color, (160, 160), interpolation=cv2.INTER_CUBIC)
-                    cv2.imwrite(str(index)+".jpg", roi_color)
+                    cv2.imwrite(os.path.join(write_dir, str(index)+".jpg"), roi_color)
+                    cv2.imshow("face", roi_color)
+                    cv2.waitKey(1)
+                    index += 1
+
 
             except:
                 print("something wrong with the picture")
+cv2.destroyAllWindows()
